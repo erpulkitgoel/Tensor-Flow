e71e4ccfd1e397f8836d8920482f6b12c01225cc
erpulkitgoel <55536715+erpulkitgoel@users.noreply.github.com>
2019-11-06 15:07:48 +0530

    Add files via upload

---
+++ Getting started with Tensor flow.py
@@ -0,0 +1,16 @@
+# How Tensorflow Works
+# Declaring variable and Tensor
+# Using place Holders and VAriables
+# Working with MAtrixs
+# Declaring Operators
+# Implementing Activation Functions
+
+# How Tensor Flow Work
+# Declaring Tensors- Basic unit of TensorFlow
+
+import tensorflow as tf
+
+# Fixed Tensors
+
+zero_tensor = tf.zeros([4,5])
+zero_tensor
